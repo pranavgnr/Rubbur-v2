143edc4cd07a1f870faf105d51e74c8a8e4707fc
pranavgnr <gnrpranav@gmail.com>
2024-11-11 07:54:45 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BookComponent } from './book/book.component';
+import { EditorComponent } from './editor/editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    BookComponent,
+    EditorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/book/book.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrl: './book.component.css'
+})
+export class BookComponent {
+  @Input() heading: string | undefined;
+
+  title:any = ''
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.title = this.heading;
+  }
+}
